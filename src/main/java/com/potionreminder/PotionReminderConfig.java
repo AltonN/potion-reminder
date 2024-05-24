2f1d60ec2c8f54c3fc0641e20a835c8b2af708a8
AltonN <hyperg3nesis@gmail.com>
2024-05-23 23:44:18 -0700

    Enable config options

--- src/main/java/com/potionreminder/PotionReminderConfig.java
+++ src/main/java/com/potionreminder/PotionReminderConfig.java
@@ -3,17 +3,71 @@ package com.potionreminder;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Units;
 
 @ConfigGroup("example")
 public interface PotionReminderConfig extends Config
 {
+
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+			keyName = "notificationOffset",
+			name = "Notification Offset",
+			description = "The amount of seconds before potion expires to notify.",
+			position = 2
 	)
-	default String greeting()
+	@Units(Units.SECONDS)
+	default int notificationOffset()
 	{
-		return "Hello";
+		return 0;
+	}
+
+	@ConfigItem(
+			keyName = "showStamina",
+			name = "Stamina",
+			description = ""
+	)
+	default boolean showStamina()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "showAntifire",
+			name = "Antifire",
+			description = ""
+	)
+	default boolean showAntifire()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "showSuperAntifire",
+			name = "Super Antifire",
+			description = ""
+	)
+	default boolean showSuperAntifire()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "showAntipoison",
+			name = "Antipoison",
+			description = ""
+	)
+	default boolean showAntipoison()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "showAntivenom",
+			name = "Antivenom",
+			description = ""
+	)
+	default boolean showAntivenom()
+	{
+		return true;
 	}
 }

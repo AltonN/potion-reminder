b363d30ab83e5108d00ef25c20d81abab56f1545
AltonN <hyperg3nesis@gmail.com>
2024-06-22 01:05:00 -0700

    Feature: Add antipoison and venom

--- src/main/java/com/potionreminder/PotionReminderPlugin.java
+++ src/main/java/com/potionreminder/PotionReminderPlugin.java
@@ -4,7 +4,6 @@ import com.google.inject.Provides;
 import static com.potionreminder.Status.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.IntUnaryOperator;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
@@ -32,6 +31,8 @@ public class PotionReminderPlugin extends Plugin
 	private static final int ANTIPOISON_MULTIPLIER = 30;
 	private static final int ANTIVENOM_MULTIPLIER = 30;
 
+	private static final int VENOM_VALUE_CUTOFF = -38;
+
 	@Inject
 	private Client client;
 
@@ -44,22 +45,39 @@ public class PotionReminderPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged event)
 	{
+		// Stamina
 		if (event.getVarbitId() == Varbits.STAMINA_EFFECT && config.showStamina())
 		{
 			final int tickDuration = event.getValue() * STAMINA_MULTIPLIER;
 			handleTimer(STAMINA, tickDuration);
 		}
 
+		// Antifire
 		if (event.getVarbitId() == Varbits.ANTIFIRE && config.showAntifire())
 		{
 			final int tickDuration = event.getValue() * ANTIFIRE_MULTIPLIER;
 			handleTimer(ANTIFIRE, tickDuration);
 		}
 
+		// Super antifire
 		if (event.getVarbitId() == Varbits.SUPER_ANTIFIRE && config.showSuperAntifire())
 		{
 			final int tickDuration = event.getValue() * SUPER_ANTIFIRE_MULTIPLIER;
 			handleTimer(SUPER_ANTIFIRE, tickDuration);
+		}
+
+		// Antipoison
+		if (event.getVarpId() == VarPlayer.POISON && event.getValue() >= VENOM_VALUE_CUTOFF && event.getValue() < 0)
+		{
+			final int tickDuration = Math.abs(event.getValue()) * ANTIPOISON_MULTIPLIER;
+			handleTimer(ANTIPOISON, tickDuration);
+		}
+
+		// Anti-venom
+		if (event.getVarpId() == VarPlayer.POISON && event.getValue() < VENOM_VALUE_CUTOFF)
+		{
+			final int tickDuration = Math.abs(event.getValue() - VENOM_VALUE_CUTOFF) * ANTIVENOM_MULTIPLIER;
+			handleTimer(ANTIVENOM, tickDuration);
 		}
 	}
 
